7a9fad6ffc54d602174f158be411e12e2ee703ed
therampagerado <office@rampagesport.eu>
2024-05-07 17:31:32 +0300

    Update years and use HTTPS for links (where needed) in license headers.
    Update years and use HTTPS for links (where needed) in license headers.

--- mem/Library/Command/Memcached.php
+++ mem/Library/Command/Memcached.php
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,7 +15,7 @@
  *
  * ><)))°> ><)))°> ><)))°> ><)))°> ><)))°> ><)))°> ><)))°> ><)))°> ><)))°>
  *
- * Sending command to memcache server via PECL memcache API http://pecl.php.net/package/memcache
+ * Sending command to memcache server via PECL memcache API https://pecl.php.net/package/memcache
  *
  * @author c.mahieux@of2m.fr
  * @since  20/03/2010
